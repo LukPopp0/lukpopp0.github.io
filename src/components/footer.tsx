5952c8f0ff6e5f8a5e437e4fcab1a10ea18dd960
Lukas Popp <luk.popp0@gmail.com>
2023-02-21 16:47:41 +0100

    Add basic layout Add sass variables

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import './footer.scss';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div>Lukas Popp - Software Developer</div>
+    </footer>
+  );
+};
---
+++ src/components/main.tsx
@@ -0,0 +1,10 @@
+import './main.scss';
+
+export const Main = () => {
+  return (
+    <div className="main">
+      <h1>Currently under maintenance</h1>
+      <h2>Come back soon</h2>
+    </div>
+  );
+};
