bfc18470ed0bfe2a1988f68d178b053350a2e77b
Lukas Popp <luk.popp0@gmail.com>
2023-02-21 22:41:48 +0100

    Update blur effect Fix not blurry bar at the bottom when scrolling

--- src/components/mysticBlur.tsx
+++ src/components/mysticBlur.tsx
@@ -3,25 +3,56 @@ import './mysticBlur.scss';
 
 export const MysticBlur = () => {
   const mysticBlobContainer = useRef<HTMLDivElement>(null);
+  const mysticBlob = useRef<HTMLDivElement>(null);
 
+  // Add event listener to follow the cursor
   useEffect(() => {
     if (!mysticBlobContainer.current) return;
+
     document.body.onpointermove = ev => {
       if (!mysticBlobContainer.current) return;
+
       const { clientX, clientY } = ev;
       mysticBlobContainer.current.animate(
         { left: `${clientX}px`, top: `${clientY}px` },
         { duration: 2000, fill: 'forwards' }
       );
     };
+
+    return () => {
+      document.body.onpointermove = null;
+    };
+  }, []);
+
+  // Add border radius animation
+  useEffect(() => {
+    if (!mysticBlob.current) return;
+
+    const animationDurationInMs = 2000;
+    mysticBlob.current.style.transition = `border-radius ${animationDurationInMs}ms cubic-bezier(0.455, 0.03, 0.515, 0.955)`;
+
+    const min = 20,
+      max = 50;
+    const interval = setInterval(() => {
+      if (!mysticBlob.current) return;
+
+      const tl = Math.floor(Math.random() * (max - min) + min);
+      const tr = Math.floor(Math.random() * (max - min) + min);
+      const br = Math.floor(Math.random() * (max - min) + min);
+      const bl = Math.floor(Math.random() * (max - min) + min);
+      const borderRadius = `${tl}% ${tr}% ${br}% ${bl}%`;
+      mysticBlob.current.style.borderRadius = borderRadius;
+    }, animationDurationInMs);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div id="mystic-blur">
       <div id="mystic-blob-container" ref={mysticBlobContainer}>
-        <div id="mystic-blob" />
+        <div id="mystic-blob" ref={mysticBlob} />
       </div>
-      <div id="mystic-blur" />
+      <div id="blur-element" />
     </div>
   );
 };
