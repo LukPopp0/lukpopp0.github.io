c666f6b809709ebdf237985438d086c6ff13c868
Lukas Popp <luk.popp0@gmail.com>
2023-03-15 13:58:38 +0100

    Add scroll blur and scale effect

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,18 +1,18 @@
-import './main.scss';
 import { Intro } from './pageParts/intro';
 import { Socials } from './pageParts/socials';
+import './main.scss';
 
 export const Main = () => {
   return (
     <div className="main">
-      <div className="page-part intro">
+      <div className="page-part intro-cntnr">
         <Intro />
       </div>
-      {/* <div className="page-part cards"></div> */}
-      {/* <div className="page-part creative-coding"></div> */}
-      {/* <div className="page-part projects"></div> */}
-      {/* <div className="page-part downloads"></div> */}
-      <div className="page-part socials">
+      {/* <div className="page-part cards-cntnr"></div> */}
+      {/* <div className="page-part creative-cntnr-coding"></div> */}
+      {/* <div className="page-part projects-cntnr"></div> */}
+      {/* <div className="page-part downloads-cntnr"></div> */}
+      <div className="page-part socials-cntnr">
         <Socials />
       </div>
     </div>
