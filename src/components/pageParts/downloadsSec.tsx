c15ef0cd652f4b12e694eab4b9aeca77207b7142
Lukas Popp <luk.popp0@gmail.com>
2023-03-17 17:26:38 +0100

    Move CV file

--- src/components/pageParts/downloadsSec.tsx
+++ src/components/pageParts/downloadsSec.tsx
@@ -1,6 +1,6 @@
 import { MagicButton } from '../magicButton';
 import './downloadsSec.scss';
-import cvPath from '../../../public/CV Lukas Popp 2023-03.pdf';
+import cvPath from '../../assets/CV Lukas Popp 2023-03.pdf';
 
 export const DownloadsSec = () => {
   return (
