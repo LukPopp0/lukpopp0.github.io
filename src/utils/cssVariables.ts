46ce3df10ff9fac294239bba23036c20649f336e
Lukas Popp <luk.popp0@gmail.com>
2025-01-25 12:52:21 -0500

    Fix build

--- src/utils/cssVariables.ts
+++ src/utils/cssVariables.ts
@@ -1,7 +1,11 @@
 // eslint-disable-next-line import/no-unresolved
 import variables from './../styles/variables.scss?inline';
 
-let relaxed = variables.split(' ').slice(1).join(' ');
+let relaxed = variables.slice(variables.indexOf('{'));
+// In the prod build, the last variable does not have a semicolon, add it back in to avoid problems
+if (!relaxed.includes(';\n}')) {
+  relaxed = relaxed.replace('}', ';}');
+}
 let strict = relaxed
   // Add " around keys
   .replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ')
