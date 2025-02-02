5003909fca77274e2ab059755f8ddb25e9cb215e
Lukas Popp <luk.popp0@gmail.com>
2025-02-02 10:30:08 -0500

    Add post bloom and ao

--- src/components/pageParts/projectsPage.tsx
+++ src/components/pageParts/projectsPage.tsx
@@ -1,10 +1,23 @@
-import { Canvas, context } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import { Lighting } from '../scene/lighting';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { VoronoiScene } from '../scene/voronoiScene';
-import { PerspectiveCamera as PCamera, Vector3 } from 'three';
+import { BackSide, PerspectiveCamera as PCamera, Vector3 } from 'three';
 import { useEffect, useRef, useState } from 'react';
 import { useTheme } from '../../utils';
+import { Bloom, EffectComposer, N8AO } from '@react-three/postprocessing';
+import { Stats } from '@react-three/drei';
+import { isBrowser } from 'react-device-detect';
+
+const SkyBox = () => {
+  const theme = useTheme();
+  return (
+    <mesh>
+      <boxGeometry args={[1000, 1000, 1000]} />
+      <meshBasicMaterial color={theme.mainColor} side={BackSide} />
+    </mesh>
+  );
+};
 
 export const ProjectsPage = () => {
   const cam = useRef<PCamera>(null);
@@ -22,16 +35,24 @@ export const ProjectsPage = () => {
     <div
       style={{
         width: '100%',
-        height: 'calc(100svh - 8rem)',
+        height: '100svh',
         position: 'relative',
       }}
     >
       <Canvas ref={canvasRef}>
+        <SkyBox />
+        <Stats />
         <color attach="background" args={[theme.mainColor]} />
         <Lighting />
         <PerspectiveCamera ref={cam} makeDefault position={[10, 20, -38]} />
         <OrbitControls enableZoom={false} enablePan={false} autoRotate={autoRotate} />
         <VoronoiScene />
+        <EffectComposer>
+          <>
+            <Bloom luminanceThreshold={0} luminanceSmoothing={0.8} height={400} />
+            {isBrowser && <N8AO halfRes color="black" aoRadius={2} intensity={1} aoSamples={6} denoiseSamples={4} />}
+          </>
+        </EffectComposer>
       </Canvas>
     </div>
   );
